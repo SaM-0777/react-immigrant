07cdfc961b502ea645f6e894ee03b4e53e3291cd
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2022-11-29 01:44:24 +0530

    sidebar fixed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ function SideBar({ data, setCountry, selectedCountry  }) {
   function onClickCountry(key) { setCountry(key) }
 
   return (
-    <div className="px-5">
+    <div className="px-4 w-[300px]">
       {/*<div className="flex items-center rounded-md border-[2px] border-cyan-300 px-2">
         <input
           className="px-2 w-[240px] h-[32px] text-sm font-poppins cursor-pointer focus:outline-none active:border-0"
@@ -19,7 +19,7 @@ function SideBar({ data, setCountry, selectedCountry  }) {
           <li
             key={i.toString()}
             onClick={() => onClickCountry(item)}
-            className={`${selectedCountry === item ? 'bg-cyan-400 text-white' : ''} w-[100%] mt-4 h-[40px] flex items-center font-poppins border-[2px] px-1 rounded-md hover:bg-cyan-300 active:bg-cyan-500 hover:text-white hover:border-0 cursor-pointer`}>
+            className={`${selectedCountry === item ? 'bg-cyan-400 text-white' : ''} w-full mt-4 h-[40px] flex items-center font-poppins border-[2px] rounded-md hover:bg-cyan-300 active:bg-cyan-500 hover:text-white hover:border-0 cursor-pointer px-1`}>
             {item}
           </li>
         ))}
